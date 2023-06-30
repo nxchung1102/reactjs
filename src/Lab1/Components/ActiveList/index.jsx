63c29eff53180a19ae98f6d7d76cc976a26121a1
DESKTOP-VVP9UTS\nxnam <nxnam1102@gmail.com>
2023-06-30 19:21:59 +0700

    add img

--- src/Lab1/Components/ActiveList/index.jsx
+++ src/Lab1/Components/ActiveList/index.jsx
@@ -5,25 +5,31 @@ import classNames from "classnames";
 ActiveList.propTypes = {
   activeList: PropTypes.array,
   onchkClick: PropTypes.func,
+  onChkChange: PropTypes.func,
 };
 ActiveList.defaultProps = {
   activeList: [],
   onchkClick: null,
+  onChkChange: null,
 };
-function ActiveList({ activeList, onchkClick }) {
+function ActiveList({ activeList, onchkClick, onChkChange }) {
   const handleClick = (active, i) => {
     if (!onchkClick) return;
     onchkClick(active, i);
+  };
+  const handleChange = (e) => {
+    if (!onChkChange) return;
+    onChkChange(e);
   };
   return (
     <div className="active-list">
       {activeList.map((active, i) => {
         return (
-          <div>
+          <div className="list-checkbox">
             <input
               type="checkbox"
               id={active.id}
-              name={active.status}
+              name={active.name}
               className={classNames({
                 "active-item": true,
                 selected: active.status === "selected",
@@ -31,8 +37,12 @@ function ActiveList({ activeList, onchkClick }) {
               onClick={() => {
                 handleClick(active, i);
               }}
+              onChange={(e) => {
+                handleChange(e);
+              }}
             />
             <label for={active.name}>{active.name}</label>
+            <img src={active.URL} alt={active.name} />
           </div>
         );
       })}
--- src/Lab1/index.jsx
+++ src/Lab1/index.jsx
@@ -10,21 +10,25 @@ function Lab1(props) {
       id: 1,
       name: "eat",
       status: "none",
+      URL: "https://i.pinimg.com/236x/34/f4/8f/34f48f5c56c938642b80b0555e5adf82.jpg",
     },
     {
       id: 2,
       name: "sleep",
       status: "none",
+      URL: "https://i.pinimg.com/236x/08/9c/af/089cafb7ed2ce7817d465ed4d0b970fd.jpg",
     },
     {
       id: 3,
       name: "code",
       status: "none",
+      URL: "https://i.pinimg.com/236x/1f/87/b2/1f87b29a2df46100a75aa86b170a21cb.jpg",
     },
   ];
   const [count, setCount] = useState(0);
   const [list2, setList2] = useState(activeList);
   const [filteredStatus, setFilteredStatus] = useState("all");
+  const [lastSelected, setLastSelected] = useState(null);
   const handleClick = (active, i) => {
     const newList2 = [...list2];
 
@@ -46,10 +50,23 @@ function Lab1(props) {
     return filteredStatus === "all";
   });
 
+  const handleChange = (e) => {
+    const checkedName = e.target.name;
+    if (e.target.checked) {
+      setLastSelected(checkedName);
+    } else {
+      setLastSelected(null);
+    }
+  };
   return (
     <div>
       <h3>so lan click {count}</h3>
-      <ActiveList activeList={renderList} onchkClick={handleClick} />
+      <h3>phan tu cuoi cung {lastSelected}</h3>
+      <ActiveList
+        activeList={renderList}
+        onchkClick={handleClick}
+        onChkChange={handleChange}
+      />
     </div>
   );
 }
